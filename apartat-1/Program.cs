733ab5fcc5cf3f19855e5bab6cfd148e5447121a
notbtc2 <bieloliver2006@gmail.com>
2023-02-20 11:38:00 +0100

    First commit

---
+++ apartat-1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Apartat_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string? input;
+            int a;
+            int b;
+            while (true)
+            {
+                Console.WriteLine("Introdueix el primer nombre :");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out a))
+                {
+                    break;
+                }
+            }
+            while (true)
+            {
+                Console.WriteLine("Introdueix el primer nombre :");
+                input = Console.ReadLine();
+                if (int.TryParse(input, out b))
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("El resultat és : " + Multiplicador(a, b));
+        }
+        
+        static int Multiplicador(int a, int b)
+        {
+            int resultat = 1;
+            for (int i = a; i <= b; i++)
+            {
+                resultat = resultat * i;
+            }
+            return resultat;
+        } 
+    }
+}
---
+++ apartat-2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Apartat_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Introdueix la paraula que desitges veures si es un palíndrom : ");
+                string? input = Console.ReadLine();
+                if (input != null)
+                {
+                    char[] arr = input.ToCharArray();
+                    bool isPalindrome = GetPalindrome(arr);
+                    if (isPalindrome)
+                    {
+                        Console.WriteLine($"{input} és un palíndrom.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{input} no és un palíndrom.");
+                        break;
+                    }
+                }
+            }
+            
+        }
+        
+        public static bool GetPalindrome(char[] arr)
+        {
+            string inputString = new string(arr);
+            string first = inputString.Substring(0, inputString.Length / 2);
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+    }
+}
